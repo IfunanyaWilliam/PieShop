9778b8772fcc5866cf70fa1962d1240c834bf028
IfunanyaWilliam <williamifunanya@gmail.com>
2022-04-13 17:09:45 +0100

    Create a project PieShop, add Class Pie, Category, IPieRepository in the Model

---
+++ PieShop/Models/Pie.cs
@@ -0,0 +1,17 @@
+﻿namespace PieShop.Models
+{
+    public class Pie
+    {
+        public int PieId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public string AllergyInformation { get; set; }
+        public decimal Price { get; set; }
+        public string ImgUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPieOfTheWeek { get; set; }
+        public bool InStock { get; set; }
+        public Category Category { get; set; }
+    }
+}
